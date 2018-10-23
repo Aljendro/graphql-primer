ac893f1c5520f001d41b70f813a6ecfdeca919fb
Alejandro Alvarado <aalvarado-NONEMP@collegeboard.org>
2018-10-22 23:15:17 -0400

    Adding comments to the student query

--- graphql/student/query.js
+++ graphql/student/query.js
@@ -7,11 +7,24 @@ module.exports.query = {
     type: StudentType,
     description: 'Returns a student by ID',
     args: {
+      // When using this query, we need to supply it an ID
       id: {
         type: new GraphQLNonNull(GraphQLID),
       },
     },
     resolve: (source, args) => {
+
+      /*
+       * Resolve a student with partial data, the student
+       * will able to retrieve the data given here
+       * and supplement it with other "network" calls later
+       * , if necessary
+       * 
+       * When passing in arguments to this query, args object
+       * will contain the values
+       * 
+       * In this case, args.id will contain the id.
+       */
       return Promise.resolve({
         id: faker.random.uuid(),
         firstName: faker.name.firstName(),
--- graphql/student/type.js
+++ graphql/student/type.js
@@ -1,17 +1,48 @@
-const { GraphQLNonNull, GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql');
+const {
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+} = require('graphql');
 const faker = require('faker');
 
+/*
+ *  This is how we create graphql objects that are essentially
+ *  nodes in our graphql graph. 
+ * 
+ *  We can connect them arbitrarily just signifying the return 
+ *  type with "type" to be another graphql type.
+ */
 module.exports.type = new GraphQLObjectType({
   name: 'StudentType',
   description: 'A student type.',
   fields: () => ({
+    /*
+     * In this case we are using the default "resolver".
+     * 
+     * This default "resolver" will look at the "source" 
+     * object and look for an "id" attribute
+     */
     id: {
       type: new GraphQLNonNull(GraphQLID),
     },
+    /*
+     * In this case we are using our own "resolver", which
+     * resembles what the default resolver will do.
+     */
     firstName: {
       type: new GraphQLNonNull(GraphQLString),
       resolve: (source, args) => source.firstName,
     },
+    /*
+     * Sometimes the source object will not contain some
+     * attributes.
+     * 
+     * We can get arbitrary values from anywhere!
+     * 
+     * This could have been a network call, cached query, 
+     * or any type of call needed to get the result.
+     */
     lastName: {
       type: new GraphQLNonNull(GraphQLString),
       resolve: (source, args) => {
